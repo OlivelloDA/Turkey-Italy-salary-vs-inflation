67eadcbb7bc3b51ba8e4974d803d5345336f4618
Claudio Olivelli <cicciobaccalaro@gmail.com>
2021-11-09 18:42:19 +0300

    Initial Update

---
+++ stremlit.R
@@ -0,0 +1,4 @@
+work_dir <- "/Users/claudioolivelli/Documents/GitHub/Python lab/Turkey-Italy-salary-vs-inflation"
+
+
+data <- data.frame(read.excel('/Users/claudioolivelli/Documents/GitHub/Python lab/Turkey-Italy-salary-vs-inflation/Inflation_rates.xlsx'))
